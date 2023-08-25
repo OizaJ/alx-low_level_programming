cec4b805e5183ce6d9d083beb25ddf198d74bc0a
OizaJ <joettajewel32@gmail.com>
2023-08-25 11:56:38 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - return number of element in linked list
+ * @h: point to the list_t list
+ * Return: number of element in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n)
+}
